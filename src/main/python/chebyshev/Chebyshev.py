da6e69e6df47c6ef9e9ab23dee540c4faa14c22a
test <mpetrashev@yahoo.com>
2019-03-12 20:04:38 +0000

    Added 2d functions support

--- src/main/python/chebyshev/Chebyshev.py
+++ src/main/python/chebyshev/Chebyshev.py
@@ -4,30 +4,56 @@ from numpy.polynomial.polyutils import mapdomain
 import numpy.polynomial.chebyshev as cheb
 from bisect import bisect_left
 import numpy as np
+from inspect import signature
+import collections.abc
 
 
 class Chebyshev:
 
-    def __init__(self, func, degree, x_domain, y_domain, y_levels, args) -> None:
-        self.func = func
+    def __init__(self, polynomials, degree, domain) -> None:
+        self.polynomials = polynomials
         self.degree = degree
-        self.y_domain = y_domain
-        self.y_levels = y_levels
-        self.interpolants = [cheb.Chebyshev.interpolate(func, degree, x_domain, [y] + args) for y in y_levels]
-        self.args = args
+        self.domain = domain
+        self.levels = mapdomain(cheb.chebpts1(degree + 1), cheb.Chebyshev.window, domain[-1])#review for >3d dimension
 
     @classmethod
-    def interpolate(cls, func, degree, domains=None, args: List=[]):
-        xcheb = cheb.chebpts1(degree+1)
-        y_levels = mapdomain(xcheb, cheb.Chebyshev.window, domains[1])
-        return cls(func, degree, domains[0], domains[1], y_levels, args)
+    def interpolate(cls, func, degree, domain=None, args: List=[]):
+        num_of_dims = len(signature(func).parameters) - len(args)
+        func = np.vectorize(func)
+        if num_of_dims == 1:
+            # Use just standard implementation
+            return cheb.Chebyshev.interpolate(func, degree, domain=domain, args=args)
+        else:
+            if not domain:
+                domain = [cheb.Chebyshev.window] * (num_of_dims-1)
+
+            y_levels = mapdomain(cheb.chebpts1(degree + 1), cheb.Chebyshev.window, domain[-1])
+            # for y in y_levels:
+            #     p = cheb.Chebyshev.interpolate(func, degree, domain[0], [y] + args)
+            #     print('x=0, y={}, p={}'.format(y,p(0)))
+            polynomials = [cheb.Chebyshev.interpolate(func, degree, domain[0], [y] + args) for y in y_levels]
+            return cls(polynomials, degree, domain)
+
+            # # Otherwise use recursive implementation
+            # xcheb = cheb.chebpts1(degree + 1)
+            # if not domain:
+            #     y_domain = cheb.Chebyshev.window
+            #     domain = [y_domain] * num_of_dims
+            #     y_levels = xcheb
+            # else:
+            #     if not isinstance(domain[0], collections.abc.Sequence):
+            #         domain = [domain]
+            #     y_domain = domain[1]
+            #     y_levels = mapdomain(xcheb, cheb.Chebyshev.window, domain[1])
+            #
+            # return cls(np.vectorize(func), degree, domain[0], y_domain, y_levels, args)
 
     def __call__(self, *args):
         x = args[0]
-
+        points = cheb.chebpts1(self.degree + 1)
         def f(y):
-            idx = bisect_left(self.y_levels, y)
-            return self.interpolants[idx](x)
+            idx = bisect_left(self.levels, y)
+            return self.polynomials[idx](x)
 
-        obj = cheb.Chebyshev.interpolate(np.vectorize(f), self.degree, self.y_domain)
-        return obj(args[1])
+        obj = cheb.Chebyshev.interpolate(np.vectorize(f), self.degree, self.domain[-1])
+        return obj(args[-1])
--- test/main/python/chebyshev/ChebyshevTest.py
+++ test/main/python/chebyshev/ChebyshevTest.py
@@ -1,56 +1,42 @@
 import math
 import unittest
-import numpy.polynomial.chebyshev as cheb
 from chebyshev.Chebyshev import Chebyshev
-import numpy as np
-# from rpy2.robjects import r
-from itertools import product
 
 from chebyshev.blackscholes import call_price
 
 
 class ChebyshevTest(unittest.TestCase):
 
-    # @classmethod
-    # def setUpClass(cls) -> None:
-    #     import rpy2.robjects.packages as rpackages
-    #     utils = rpackages.importr('utils')
-    #     if not rpackages.isinstalled('chebpol'):
-    #         utils.install_packages('chebpol', repos='http://cran.us.r-project.org')
-
-    def testPolynom(self):
-        # # check Chebyshev points
-        # r_cheb_pts = list(r('library(chebpol)\n' +
-        #                   'chebknots(11,intervals=c(-1,1))')[0])
-        # r_cheb_pts.reverse()
-        # cheb_pts = cheb.chebpts1(11)
-        # np.testing.assert_almost_equal(r_cheb_pts,cheb_pts)
-
-        #check function values on Chebyshev points
+    def testPolynomial(self):
         def f(x):
             return x*x + 2
+        # check 1d function without domains
+        obj = Chebyshev.interpolate(f, 10)
+        self.assertAlmostEqual(2.25, obj(.5))
 
-        obj = cheb.Chebyshev.interpolate(np.vectorize(f), 10, domain=[0, 100])
-        self.assertAlmostEqual(27, obj(5), delta=0.001)
-        # r_values = list(r('f <- function(x) x*x + 2\n'
-        #                   + 'evalongrid(f,11,intervals=c(-1,1))') )
-        # np.testing.assert_almost_equal(r_values,obj(cheb_pts))
+        # and with domains
+        obj = Chebyshev.interpolate(f, 10, domain=[0, 100])
+        self.assertAlmostEqual(27, obj(5))
 
-        #check Chebyshev coefficients
-        # r_coefs = r('chebcoef( evalongrid(f,11,intervals=c(-1,1)) )')
-        # obj = cheb.Chebyshev(r_coefs)
-        # np.testing.assert_almost_equal(r_values,obj(cheb_pts))
-
-    def test2DPolynom(self):
+    def test2DPolynomial(self):
         def g(x,y):
             return x + y * 10
-        # r_value = list(r("library(chebpol)\n"
-        #               + "g <- function(x) x[1]  + x[2]*10\n"
-        #               + "ch <- ipol(g,dims=c(5,5),method='cheb')\n"
-        #               + "ch(c(2.,1.))") )
-        # self.assertAlmostEqual(g(2.,1.),r_value[0])
-        obj = cheb.Chebyshev.interpolate(np.vectorize(g), 10, domain=[[0, 100],[0, 10]])
-        self.assertAlmostEqual(g(2.,1.),obj(2.,1.))
+        # check 2d function without domains
+        obj = Chebyshev.interpolate(g, 10)
+        self.assertAlmostEqual(g(.2, .1), obj(.2, .1))
+        # and with domains
+        obj = Chebyshev.interpolate(g, 10, domain=[[-10, 10], [-100, 100]])
+        self.assertAlmostEqual(g(1., 2.), obj(1., 2.))
+
+    def test3DPolynomial(self):
+        def h(x, y, z):
+            return 100 * x + y * 10 + z
+        # check 3d function without domains
+        # obj = Chebyshev.interpolate(h, 10)
+        # self.assertAlmostEqual(h(.2, .1,.3), obj(.2, .1,.3))
+        # and with domains
+        obj = Chebyshev.interpolate(h, 10, domain=[[-10, 10], [-100, 100], [-1000, 1000]])
+        self.assertAlmostEqual(h(1., 2., 3.), obj(1., 2., 3.))
 
     # def testNDimension(self):
     #     r_coefs = np.asarray(r('library(chebpol)\n'
@@ -62,34 +48,14 @@ class ChebyshevTest(unittest.TestCase):
     def test2DConvexFunc(self):
         def g(x, y):
             return math.sin(x) + math.cos(2 * y) + x * y
-        obj = cheb.Chebyshev.interpolate(np.vectorize(g), 10)
+        #check 2d function without domains
+        obj = Chebyshev.interpolate(g, 10)
         self.assertAlmostEqual(g(.4, .5), obj(.4, .5))
         self.assertAlmostEqual(g(.5, .4), obj(.5, .4))
-        # r_value = list(r("library(chebpol)\n"
-        #               + "g <- function(x) sin( x[1] )  + cos( 2 * x[2] )\n"
-        #               + "ch <- ipol(g,dims=c(10,10),method='cheb')\n"
-        #               + "ch(c(.5,.4))") )
-        # self.assertAlmostEqual(g(.5,.4),r_value[0],delta=0.000001)
-        #
-        # cheb_pts = cheb.chebpts1(10)[::-1]
-        # num_of_arguments = 2
-        # grid = [x[::-1] for x in product(cheb_pts, repeat=num_of_arguments)]
-        # values = [g(*args) for args in grid]
-        # values = [ values[0] ] + values
-        # code = 'values <- c(' + ','.join(np.char.mod('%.10f', values)) + ')\n' \
-        #        + 'make.g <- function() {\n'  \
-        #        + '  i <- 0\n' \
-        #        + '  g <- function(x){\n' \
-        #        + '      i <<- i + 1\n' \
-        #        + '      return (values[ i ])\n'\
-        #        + '  }\n' \
-        #        + '  return (g)\n' \
-        #        + '}\n' \
-        #        + 'g <- make.g()\n'
-        # r_value = list(r(code
-        #               + "ch <- ipol(g,dims=c(10,10),method='cheb')\n"
-        #               + "ch(c(.4,.5))") )
-        # self.assertAlmostEqual(g(.4,.5),r_value[0],delta=0.000001)
+        #and with domains
+        obj = Chebyshev.interpolate(g, 18, domain=[[-math.pi, math.pi], [-math.pi, math.pi]])
+        self.assertAlmostEqual(g(1.4, 1.5), obj(1.4, 1.5))
+        self.assertAlmostEqual(g(1.5, 1.4), obj(1.5, 1.4))
 
     def testBlackScholes(self):
         price = call_price(23.75, 15., 0.01, 0.35, 0.5)
@@ -98,13 +64,18 @@ class ChebyshevTest(unittest.TestCase):
 
     def test1D_BlackScholes(self):
         x_r_sigma_t = [15., 0.01, 0.35,  0.5]
-        obj = cheb.Chebyshev.interpolate(np.vectorize(call_price), 25, [0, 100], x_r_sigma_t)
+        obj = Chebyshev.interpolate(call_price, 25, [0, 100], x_r_sigma_t)
         price = call_price(23.75, *x_r_sigma_t)
         self.assertAlmostEqual(price, obj(23.75), delta=0.001)
 
     def test2D_BlackScholes(self):
         f = lambda s, sigma: call_price(s, 15., 0.01, sigma, 0.5)
-        obj = Chebyshev.interpolate(np.vectorize(f), 25, [[10, 100], [0.1, 1.]])
+        obj = Chebyshev.interpolate(f, 25, [[10, 100], [0.1, 1.]])
         price = obj(23.75, 0.35)
+        self.assertAlmostEqual(8.879159263714124, price, delta=0.001)
+
+    def testND_BlackScholes(self):
+        obj = Chebyshev.interpolate(call_price, 25, [[10, 100], [10, 100], [0., .2], [0.1, 1.], [0.5, 5]])
+        price = obj(23.75, 15., 0.01, 0.35,  0.5)
         self.assertAlmostEqual(8.879159263714124, price, delta=0.001)
 
