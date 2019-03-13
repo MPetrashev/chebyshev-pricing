2ef99c5e4543c5f7c6b1e828d72001b437be4927
MPetrashev <mpetrashev@mail.ru>
2019-03-13 15:29:54 +0000

    Implemented for 3D functions

--- src/main/python/chebyshev/Chebyshev.py
+++ src/main/python/chebyshev/Chebyshev.py
@@ -5,26 +5,53 @@ import numpy.polynomial.chebyshev as cheb
 from bisect import bisect_left
 import numpy as np
 from inspect import signature
+from functools import partial
 import collections.abc
 
 
 class Chebyshev:
 
-    def __init__(self, polynomials, degree, domain) -> None:
+    def __init__(self, polynomials, degree, domain,chebpts) -> None:
         self.polynomials = polynomials
         self.degree = degree
         self.domain = domain
-        self.levels = mapdomain(cheb.chebpts1(degree + 1), cheb.Chebyshev.window, domain[-1])#review for >3d dimension
+        self.chebpts = chebpts
+        self.levels = mapdomain(chebpts, cheb.Chebyshev.window, domain[-1])#review for >3d dimension
+
+    def _get_polynomial_cube(self, level):
+        ### Returns the cube of polynomials for all sub-levels starting from level `level`
+        ###
+        return 0
+
+    @staticmethod
+    def _chebinterpolate(chebpts,values):
+        order = len(chebpts)
+        m = cheb.chebvander(chebpts, order-1)
+        c = np.dot(m.T, values)
+        c[0] /= order
+        c[1:] /= 0.5 * order
+
+        return c
+
+    @staticmethod
+    def _interpolate_and_value(chebpts, values, x, domain):
+        coef = Chebyshev._chebinterpolate(chebpts, values)
+        f = cheb.Chebyshev(coef, domain=domain)
+        return f(x)
 
     @classmethod
     def interpolate(cls, func, degree, domain=None, args: List=[]):
         num_of_dims = len(signature(func).parameters) - len(args)
         chebpts = cheb.chebpts1(degree + 1)
-        func = np.vectorize(func)
         if num_of_dims == 1:
+            if not domain:
+                domain = cheb.Chebyshev.window
             # Use just standard implementation
-            return cheb.Chebyshev.interpolate(func, degree, domain=domain, args=args)
+            values = np.vectorize(lambda x: func(x, *args))(mapdomain(chebpts, cheb.Chebyshev.window, domain))
+            coef = Chebyshev._chebinterpolate(chebpts, values)
+            return cheb.Chebyshev(coef, domain=domain)
         else:
+            func = np.vectorize(func)
             if not domain:
                 domain = [cheb.Chebyshev.window] * (num_of_dims)
 
@@ -37,32 +64,23 @@ class Chebyshev:
                     return [create_polynomials(level-1, [y] + args) for y in levels]
 
             polynomials = create_polynomials(num_of_dims-1, args)
-            return cls(polynomials, degree, domain)
+            return cls(polynomials, degree, domain,chebpts)
 
-            # # Otherwise use recursive implementation
-            # xcheb = chebpts
-            # if not domain:
-            #     y_domain = cheb.Chebyshev.window
-            #     domain = [y_domain] * num_of_dims
-            #     y_levels = xcheb
-            # else:
-            #     if not isinstance(domain[0], collections.abc.Sequence):
-            #         domain = [domain]
-            #     y_domain = domain[1]
-            #     y_levels = mapdomain(xcheb, cheb.Chebyshev.window, domain[1])
-            #
-            # return cls(np.vectorize(func), degree, domain[0], y_domain, y_levels, args)
+    @staticmethod
+    def _get_sub_grid(x,polynomials):
+        if isinstance(polynomials[0],list):
+            return [Chebyshev._get_sub_grid(x,sub_polynomials) for sub_polynomials in polynomials]
+        else:
+            return [p(x) for p in polynomials]
 
     def __call__(self, *args):
         x = args[0]
-        points = cheb.chebpts1(self.degree + 1)
-        def f(y):
-            idx = bisect_left(self.levels, y)
-            return self.polynomials[idx](x)
+        y = args[1]
 
+        values = Chebyshev._get_sub_grid(x, self.polynomials)
         if len(args) == 2:
-            obj = cheb.Chebyshev.interpolate(np.vectorize(f), self.degree, self.domain[-1])
-            return obj(args[-1])
+            return Chebyshev._interpolate_and_value(self.chebpts, values, y, self.domain[1])
         else:
-            return None
-
+            z = args[2]
+            values2 = [Chebyshev._interpolate_and_value(self.chebpts, v, y, self.domain[1]) for v in values]
+            return Chebyshev._interpolate_and_value(self.chebpts, values2, z, self.domain[2])
